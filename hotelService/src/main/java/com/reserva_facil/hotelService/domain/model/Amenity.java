6e344df6088f605399da3ce73a43bce2f8bfb18d
venicios <marcos.chaves@detran.pi.gov.br>
2024-08-21 10:57:34 -0300

    Criando as Classes Room e Amenity petencente ao Microserviço HotelService

--- hotelService/src/main/java/com/reserva_facil/hotelService/domain/model/Amenity.java
+++ hotelService/src/main/java/com/reserva_facil/hotelService/domain/model/Amenity.java
@@ -1,7 +1,29 @@
 package com.reserva_facil.hotelService.domain.model;
 
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Amenity {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    private Set<Hotel> hotels;
 
 }
 
--- hotelService/src/main/java/com/reserva_facil/hotelService/domain/model/Room.java
+++ hotelService/src/main/java/com/reserva_facil/hotelService/domain/model/Room.java
@@ -1,4 +1,34 @@
 package com.reserva_facil.hotelService.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Room {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String roomNumber;
+
+    @NotBlank
+    @Column(nullable = false)
+    private int capacity;
+
+    @NotBlank
+    @Column(nullable = false)
+    private double price;
+
+    @ManyToOne
+    private Hotel hotel;
 }
