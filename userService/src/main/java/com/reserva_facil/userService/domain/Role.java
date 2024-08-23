344fe85919a7ed4099e05933fa9c9a86a53158a6
venicios <marcos.chaves@detran.pi.gov.br>
2024-08-23 12:39:49 -0300

    -  Implementação da entidade Role, que representa os papéis atribuídos aos usuários. -  Atualização da entidade User para incluir uma relação muitos-para-muitos com a entidade Role.

---
+++ userService/src/main/java/com/reserva_facil/userService/domain/Role.java
@@ -0,0 +1,21 @@
+package com.reserva_facil.userService.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Role {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String roleName;
+}
---
+++ userService/src/main/java/com/reserva_facil/userService/domain/User.java
@@ -0,0 +1,34 @@
+package com.reserva_facil.userService.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class User {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String username;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToMany
+    private Set<Role> roles;
+}
